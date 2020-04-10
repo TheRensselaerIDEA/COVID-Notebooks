7f3d8bf0202e1d27f390529fb11f7e69b7d18a4b
Thomas Hopkins <thomashopkins000@gmail.com>
2020-04-10 15:52:06 -0400

    deleted temporary data file

--- nytimes_downloader.R
+++ nytimes_downloader.R
@@ -6,7 +6,7 @@ library(tidyverse)
 # (You must edit the date below in two places to be yesterday's date)
 # so if today is 4/10/2020 use 2020-04-09
 currDate <- "2020-04-09"
-dateURL.1 <- "NYTIMES_04-09-2020.csv"
+dateURL.1 <- "NYTIMES_temporary.csv"
 dateURL.2 <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 
 # Write raw data to file system; use date-based name syntax as above
@@ -21,10 +21,10 @@ covid_data_counties <- todays_raw_data %>%
   filter(date == currDate) %>%
   select(fips,county,cases,deaths)
 
-# change column names to match app
+# Change column names to match app
 colnames(covid_data_counties) <- c('FIPS','NAME','covid19_cases','covid19_deaths')
 
-# create a new york state row
+# Create a new york state row
 covid_data_new_york <- covid_data_counties %>%
   summarize(covid19_cases=sum(covid19_cases), covid19_deaths=sum(covid19_deaths))
 
@@ -34,9 +34,13 @@ covid_data_new_york <- covid_data_new_york[,c("FIPS", "NAME", "covid19_cases", "
 
 covid_data_counties <- data.frame(rbind(covid_data_new_york, covid_data_counties))
 
-# make backup of existing data
+# Make backup of existing data
 write_csv(read_csv("data/csv/state_covid_mortality.csv"),"data/csv/state_covid_mortality.csv.bak")
 
-# write data frame to file system
+# Write data frame to file system
 write_csv(covid_data_counties, "data/csv/state_covid_mortality.csv")
+
+# Delete raw data file because it is large and updated daily
+if (file.exists(paste0('data/csv/', dateURL.1)))
+  file.remove(paste0('data/csv/', dateURL.1))
 
