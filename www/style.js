4d851b7589b650fb17c6c97d6f9703a5a57d96bd
Jose Figueroa <josefigueroa168@gmail.com>
2020-05-14 12:20:04 -0400

    fix recursive folder problem

--- www/style.js
+++ www/style.js
@@ -8,26 +8,31 @@ var ids = {
   "sidebar_us_cardio": "#mainpanel_us_cardio",
   "sidebar_ny_mort": "#mainpanel_ny_mort",
   "sidebar_ny_cases": "#mainpanel_ny_cases",
-  "sidebar_ny_CoT": "#mainpanel_ny_CoT", 
+  "sidebar_ny_CoT": "#mainpanel_ny_CoT",
+  "sidebar_ny_CoT_region": "#mainpanel_ny_CoT_region",
   "sidebar_ny_det": "#mainpanel_ny_det", 
   "sidebar_ny_CoT_rates": "#mainpanel_ny_CoT_rates", 
+  "sidebar_ny_CoT_rates_regions": "#mainpanel_ny_CoT_rates_regions",
   "sidebar_ny_race": "#mainpanel_ny_race", 
   "sidebar_us_mort_race": "#mainpanel_us_mort_race", 
   "sidebar_ct_race": "#mainpanel_ct_race"
 };
 
+
 function resize_plot() {
   for (var key in ids) {
+  var sidebar0 = document.getElementById("sidebar_us_mort");
   var sidebar1 = document.getElementById(key);
   if(sidebar1) {
-    var positionInfo = sidebar1.getBoundingClientRect();
-    var height = positionInfo.height;
+    var positionInfo0 = sidebar0.getBoundingClientRect();
+    var positionInfo1 = sidebar1.getBoundingClientRect();
+    var height = Math.max(positionInfo0.height,positionInfo1.height);
     $(ids[key]).height(height);
   }
   }
 }
 
-//$("#mainPanel_us_mort").on("resize", resize_plot);
 window.addEventListener("resize", resize_plot);
 window.addEventListener("click", resize_plot);
+window.addEventListener("scroll", resize_plot);
 resize_plot();
