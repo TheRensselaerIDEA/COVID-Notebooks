b66dfcbd137b63836d784f2fb9271ea19e159315
TiburonB <tiburonbenavides@gmail.com>
2020-06-08 08:16:23 -0400

    first attempt bash script parallelizer

---
+++ parana.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+declare -a file_names=("aggregate_beds.R" "log_pop.R" "smoking_bmi.R" "lt_10_confirmed.R" "spatial_correlation_gamm.R" "beds.R" "main_analyses.R" "temp_humidity.R" "cli.R" "non_linear_pm.R" "tested.R" "exclude_NY_metro.R" "nonlog_pop.R" "urban.R" "fixed_NB.R" "outbreak.R" "zero_inflated.R")
+
+for file in "${file_names[@]}"; do {
+  echo `Rscript Analyses/$file` &
+} done
