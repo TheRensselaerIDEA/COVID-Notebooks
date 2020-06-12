cd3336cd6f5be7ef83798f6abc0ef291e1789c41
Mouhongkiii <mouhongxi@gmail.com>
2020-06-12 09:08:50 -0400

    Formatting states and counties to FIPS

--- Preprocessing_1.R
+++ Preprocessing_1.R
@@ -10,7 +10,10 @@ source("./Modules/Source.R")
 #COPD_nys_county <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/COPD_nys_county.csv"))
 
 COPD_national <- read_csv("data/COPD_national.csv")
+COPD_national$FIPS <- fips(COPD_national$State)
+
 COPD_nys_county <- read_csv("data/COPD_nys_county.csv")
+COPD_nys_county$FIPS <- fips("NY", COPD_nys_county$County)
 
 #------------------------------------------------------------------------------------------------------------------------------------------
 # High Cholesterol (Hyperlipidemia) datasets
@@ -19,7 +22,11 @@ COPD_nys_county <- read_csv("data/COPD_nys_county.csv")
 #highcholesterol_data_nyscounty <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/highcholesterol_data_nyscounty.csv"))
 
 highcholesterol_data_states <- read_csv("data/highcholesterol_data_states.csv")
+highcholesterol_data_states$FIPS <- fips(highcholesterol_data_states$`State Name`)
+
 highcholesterol_data_nyscounty <- read_csv("data/highcholesterol_data_nyscounty.csv")
+highcholesterol_data_nyscounty$FIPS[highcholesterol_data_nyscounty$County != "New York City"] <- fips("NY", highcholesterol_data_nyscounty$County)
+highcholesterol_data_nyscounty$FIPS[highcholesterol_data_nyscounty$County == "New York City"] <- 'NA'
 
 #------------------------------------------------------------------------------------------------------------------------------------------
 # Age 65+ datasets
@@ -28,4 +35,7 @@ highcholesterol_data_nyscounty <- read_csv("data/highcholesterol_data_nyscounty.
 #age65plus_data_nyscounty <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/age65plus_data_nyscounty.csv"))
 
 age65plus_data_states <- read_csv("data/age65plus_data_states.csv")
+age65plus_data_states$FIPS <- fips(age65plus_data_states$Geography)
+
 age65plus_data_nyscounty <- read_csv("data/age65plus_data_nyscounty.csv")
+age65plus_data_nyscounty$FIPS[age65plus_data_nyscounty$County != "New York" & !is.na(age65plus_data_nyscounty$County)] <- fips("NY", age65plus_data_nyscounty$County)
