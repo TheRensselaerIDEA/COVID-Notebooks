8cbdf162fef6d6bfd6ebeb8bac5fb2140dfa7c47
Mouhongkiii <mouhongxi@gmail.com>
2020-06-15 10:10:26 -0400

    imported new datasets

--- Preprocessing_1.R
+++ Preprocessing_1.R
@@ -6,36 +6,51 @@ source("./Modules/Source.R")
 #------------------------------------------------------------------------------------------------------------------------------------------
 # COPD datasets
 
-#COPD_national <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/COPD_national.csv"))
-#COPD_nys_county <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/COPD_nys_county.csv"))
+COPD_national <- read_csv("Data/COPD_data_states.csv")
 
-COPD_national <- read_csv("data/COPD_national.csv")
-COPD_national$FIPS <- fips(COPD_national$State)
-
-COPD_nys_county <- read_csv("data/COPD_nys_county.csv")
-COPD_nys_county$FIPS <- fips("NY", COPD_nys_county$County)
+COPD_nys_county <- read_csv("Data/COPD_data_nyscounty.csv")
 
 #------------------------------------------------------------------------------------------------------------------------------------------
 # High Cholesterol (Hyperlipidemia) datasets
 
-#highcholesterol_data_states <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/highcholesterol_data_states.csv"))
-#highcholesterol_data_nyscounty <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/highcholesterol_data_nyscounty.csv"))
+highcholesterol_data_states <- read_csv("Data/highcholesterol_data_states.csv")
 
-highcholesterol_data_states <- read_csv("data/highcholesterol_data_states.csv")
-highcholesterol_data_states$FIPS <- fips(highcholesterol_data_states$`State Name`)
-
-highcholesterol_data_nyscounty <- read_csv("data/highcholesterol_data_nyscounty.csv")
-highcholesterol_data_nyscounty$FIPS[highcholesterol_data_nyscounty$County != "New York City"] <- fips("NY", highcholesterol_data_nyscounty$County)
-highcholesterol_data_nyscounty$FIPS[highcholesterol_data_nyscounty$County == "New York City"] <- 'NA'
+highcholesterol_data_nyscounty <- read_csv("Data/highcholesterol_data_nyscounty.csv")
 
 #------------------------------------------------------------------------------------------------------------------------------------------
 # Age 65+ datasets
 
-#age65plus_data_states <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/age65plus_data_states.csv"))
-#age65plus_data_nyscounty <- read_csv(text=getURL("https://raw.githubusercontent.com/Mouhongkiii/COVID-Notebooks/PM_COVID/data/age65plus_data_nyscounty.csv"))
+age65plus_data_states <- read_csv("Data/age65plus_data_states.csv")
 
-age65plus_data_states <- read_csv("data/age65plus_data_states.csv")
-age65plus_data_states$FIPS <- fips(age65plus_data_states$Geography)
+age65plus_data_nyscounty <- read_csv("Data/age65plus_data_nyscounty.csv")
 
-age65plus_data_nyscounty <- read_csv("data/age65plus_data_nyscounty.csv")
-age65plus_data_nyscounty$FIPS[age65plus_data_nyscounty$County != "New York" & !is.na(age65plus_data_nyscounty$County)] <- fips("NY", age65plus_data_nyscounty$County)
+age65plus_data_uscounty <- read_csv("Data/age65plus_data_uscounty.csv")
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# Heart Disease / Cardiovascular Disease
+
+heartdisease_deaths_data_states <- read_csv("Data/HeartDisease_DeathRate_States_CDC_2015.csv")
+
+heartdisease_deaths_data_nyscounty <- read_csv("Data/heartdiseasedeaths_data_nyscounty.csv")
+
+heartdisease_data_nyscounty <- read_csv("Data/heartdisease_data_nyscounty.csv")
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# Adult Obesity
+
+obesity_data_states <- read_csv("Data/obesity_data_states.csv")
+
+obesity_data_nyscounty <- read_csv("Data/diabetesobesity_data_nyscounty.csv")
+obesity_data_nyscounty <- subset(obesity_data_nyscounty, select = -c(pct_Adults_with_Diabetes))
+
+obesity_data_uscounty <- read_csv("Data/adultobesity_data_uscounty.csv")
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# Diabetes
+
+diabetes_data_states <- read_csv("Data/diabetes_data_states.csv")
+
+diabetes_data_nyscounty <- read_csv("Data/diabetesobesity_data_nyscounty.csv")
+diabetes_data_nyscounty <- subset(diabetes_data_nyscounty, select = -c(pct_Adults_with_Obesity))
+
+diabetes_data_uscounty <- read_csv("Data/diabetes_data_uscounty.csv")
