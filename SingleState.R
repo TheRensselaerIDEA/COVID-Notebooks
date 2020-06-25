7c70b60820eebf157bd41a2d08a49af67498a29d
Mouhongkiii <mouhongxi@gmail.com>
2020-06-25 15:54:40 -0400

    Per state analyses results

--- SingleState.R
+++ SingleState.R
@@ -13,13 +13,17 @@ for (name in names(statesplit)) {
   }
 }
 
-names(statesplit)
+states <- names(statesplit)
 
 for (name in names(statesplit)) {
+  if (name %in% c("NM", "RI"))
+    next
   state_data <- statesplit[[name]]
+  state_data <- subset(state_data, select = c(fips, Deaths, COPD, pct_blk, q_popdensity, medhouseholdincome, education, beds, population, pct_obesity, pct_age65, pct_diabetes))
   model <- glm.nb(Deaths ~ scale(pct_blk) + factor(q_popdensity)
                        + scale(log(medhouseholdincome))+scale(education)
                        + scale(beds/population)+scale(pct_obesity)
+                       + scale(COPD)
                        + scale(pct_age65)+scale(pct_diabetes)
                        + offset(log(population)), data=state_data)
   result <- paste(name, ".summary", sep = "")
@@ -27,3 +31,5 @@ for (name in names(statesplit)) {
   fname = paste("./StateSummaries/",result,".rda",sep="")
   do.call(save, list(result, file=fname))
 }
+
+#coefficient.matrix<-exp(summary[["coefficients"]][1:13])
