aeb131619857b85adce24a1ec92f0a2609afa810
Mouhongkiii <mouhongxi@gmail.com>
2020-08-19 10:09:29 -0400

    Deleted cardiovascular death rate in GWAS; Added social determinants zoom in files

--- GWAS/parasdwas.sh
+++ GWAS/parasdwas.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # LEFT TO RUN:
-# declare -a var_names=("Infant Mortality Rate" "Cancer.death_rate" "Suicide Rate (Age-Adjusted)" "Assault.death_rate" "Cardiovascular.death_rate"
-#                           "Motor Vehicle Mortality Rate" "Drug Overdose Mortality Rate" "All.Cause.death_rate" "PediatricAsthma" "AdultChronicLungDisease"
+# declare -a var_names=("Infant Mortality Rate" "Cancer.death_rate" "Suicide Rate (Age-Adjusted)" "Assault.death_rate" "Motor Vehicle Mortality Rate" 
+                            "Drug Overdose Mortality Rate" "All.Cause.death_rate" "PediatricAsthma" "AdultChronicLungDisease"
 #                       "% Not Proficient in English" "% Insufficient Sleep" "% Unemployed" "% Drive Alone to Work" "% Long Commute - Drives Alone"
 #                           "% Food Insecure" "% With Access to Exercise Opportunities" "% Frequent Mental Distress" "% Smokers" "% Excessive Drinking"
 #                           "Overcrowding" "% less than 18 years of age" "% Homeowners" "% Severe Housing Cost Burden"
