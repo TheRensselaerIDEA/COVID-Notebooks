262201cc2520c88b2ed03ba00a8f1fdb36d7ed3b
Mouhongkiii <mouhongxi@gmail.com>
2020-08-24 18:59:33 -0400

    Organized Source file; Modified National High Risk Model

--- Social_Determinants/Analyses/NationalModel.R
+++ Social_Determinants/Analyses/NationalModel.R
@@ -1,63 +1,41 @@
 knitr::opts_chunk$set(echo = TRUE)
 knitr::opts_knit$set(root.dir = "../")
 
-library("MASS")
-library("lme4")
-library("glmmTMB")
-library("gamm4")
-library('caret')
-library('blmeco')
-library(pROC)
-library(VineCopula)
-library("PerformanceAnalytics")
-library(glmmTMB)
-library(ggplot2)
-library(cvms)
-library(sgof)
-library(tidyverse)
-library(kableExtra)
+source("./Social_Determinants/Modules/Source.R")
 
 # set datafile from parallelism
 # args <- commandArgs()
 # datafile = args[6]
 
 # set datefile by hand
-# datafile = '07-06-2020data-2.Rds'
-datafile = './Preprocessing_FTS_Outputs/07-12-2020data.Rds'
+datafile = './Social_Determinants/Preprocessing_FTS_Outputs/07-05-2020data.Rds'
 
-aggregate_pm_census_cdc_test_beds_age_diabete_obesity_heart<-readRDS(datafile)
-
-# aggregate_pm_census_cdc_test_beds_age_diabete_obesity_heart <- subset(aggregate_pm_census_cdc_test_beds_age_diabete_obesity_heart, 
-#                                                                       select = c(Deaths, `% Hispanic`, `% Black`, `% Asian`, `% Non-Hispanic White`, `% Native Hawaiian/Other Pacific Islander`, q_popdensity, 
-#                                                                                  `Median Household Income`, date_since_social, date_since, date_since_reopen, date_since_reclosure, date_since_mask, 
-#                                                                                  pct_obesity, pct_age65, pct_diabetes, LungCancer, COPD, AdultAsthma, PediatricAsthma, All.Cause.death_rate, state, population))
+aggregate_pm_census_cdc_test_beds_age_diabete_obesity_heart <- readRDS(datafile)
 
 combined.mode.nb.random.off.main = glmer.nb(Deaths 
-                                            # ~ scale(`% Hispanic`) + scale(`% Black`) + scale(`% Asian`) 
-                                            # + scale(`% Non-Hispanic White`) + scale(`% Native Hawaiian/Other Pacific Islander`)
-                                            ~ scale(`hispanic`) + scale(`pct_blk`) + scale(`pct_asian`) 
-                                            + scale(`pct_white`) + scale(`pct_native`)
+                                            ~ scale(hispanic) + scale(pct_blk) + scale(pct_asian) + scale(pct_white) + scale(pct_native)
                                             + factor(q_popdensity)
-                                            + scale(log(`Median Household Income`))
-                                            + scale(log(medhouseholdincome))
-                                            + scale(date_since_social) 
-                                            + scale(date_since) 
+                                            + scale(log(medhouseholdincome)) 
+                                            + scale(education) 
+                                            + scale(beds/population)
+                                            + scale(date_since_social)
+                                            + scale(date_since)
                                             + scale(date_since_reopen)
-                                            + scale(date_since_reclosure) 
+                                            + scale(date_since_reclosure)
                                             + scale(date_since_mask)
                                             + scale(pct_obesity)
-                                            + scale(pct_age65) + scale(pct_diabetes)
+                                            + scale(pct_age65) 
+                                            + scale(pct_diabetes)
                                             + scale(LungCancer)
                                             + scale(COPD)
                                             + scale(AdultAsthma)
-                                            + scale(PediatricAsthma)
-                                            + scale(All.Cause.death_rate)
+                                            + scale(Cardiovascular.death_rate)
                                             + (1|state)
                                             + offset(log(population)), data = aggregate_pm_census_cdc_test_beds_age_diabete_obesity_heart, 
                                             )
 # save results in parallelism
-date = substr(datafile, 29,33)
+date = substr(datafile, 49,53)
 assign(date, summary(combined.mode.nb.random.off.main))
-fname = paste("./TemporalResults/NationalModel/",date,".rda",sep="")
+fname = paste("./Social_Determinants/TemporalResults/NationalModel/",date,".rda",sep="")
 do.call(save, list(date, file=fname))
 s<-summary(combined.mode.nb.random.off.main)
--- Social_Determinants/Analyses/README.md
+++ Social_Determinants/Analyses/README.md
@@ -3,8 +3,8 @@ This is the folder containing national and state models for analysis.
 
 # National Model
 
-[`NationalModel.R`](https://github.com/TheRensselaerIDEA/COVID-Notebooks/tree/Social_Determinants/Analyses/NationalModel.R) includes the national model for social determinants analysis.
+[`NationalModel.R`](https://github.com/TheRensselaerIDEA/COVID-Notebooks/tree/MergeSD/Social_Determinants/Analyses/NationalModel.R) includes the national model for social determinants analysis.
 
 # State Model
 
-[`StateModel.R`](https://github.com/TheRensselaerIDEA/COVID-Notebooks/tree/Social_Determinants/Analyses/StateModel.R) includes the national model for social determinants analysis.
+[`StateModel.R`](https://github.com/TheRensselaerIDEA/COVID-Notebooks/tree/MergeSD/Social_Determinants/Analyses/StateModel.R) includes the national model for social determinants analysis.
